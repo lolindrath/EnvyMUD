4dacb1fa20caa504033e861a645b28ecc8e207bf
Andy Williams <lolindrath@gmail.com>
2014-04-28 17:09:28 -0400

    Make it compile on Linux

--- src/comm.c
+++ src/comm.c
@@ -158,18 +158,18 @@ int	socket		args( ( int domain, int type, int protocol ) );
 #endif
 
 #if	defined( linux )
-int	accept		args( ( int s, struct sockaddr *addr, int *addrlen ) );
-int	bind		args( ( int s, struct sockaddr *name, int namelen ) );
-int	close		args( ( int fd ) );
-int	getpeername	args( ( int s, struct sockaddr *name, int *namelen ) );
-int	getsockname	args( ( int s, struct sockaddr *name, int *namelen ) );
-int	gettimeofday	args( ( struct timeval *tp, struct timezone *tzp ) );
-int	listen		args( ( int s, int backlog ) );
-int	read		args( ( int fd, char *buf, int nbyte ) );
-int	select		args( ( int width, fd_set *readfds, fd_set *writefds,
-			    fd_set *exceptfds, struct timeval *timeout ) );
-int	socket		args( ( int domain, int type, int protocol ) );
-int	write		args( ( int fd, char *buf, int nbyte ) );
+//int	accept		args( ( int s, struct sockaddr *addr, int *addrlen ) );
+//int	bind		args( ( int s, struct sockaddr *name, int namelen ) );
+//int	close		args( ( int fd ) );
+//int	getpeername	args( ( int s, struct sockaddr *name, int *namelen ) );
+//int	getsockname	args( ( int s, struct sockaddr *name, int *namelen ) );
+//int	gettimeofday	args( ( struct timeval *tp, struct timezone *tzp ) );
+//int	listen		args( ( int s, int backlog ) );
+//int	read		args( ( int fd, char *buf, int nbyte ) );
+//int	select		args( ( int width, fd_set *readfds, fd_set *writefds,
+////			    fd_set *exceptfds, struct timeval *timeout ) );
+//int	socket		args( ( int domain, int type, int protocol ) );
+//int	write		args( ( int fd, char *buf, int nbyte ) );
 #endif
 
 #if	defined( macintosh )
