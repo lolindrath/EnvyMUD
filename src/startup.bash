3cf7bf8df0afd8b4ee5d0ee8baf13f0db19ff8c4
Andy Williams <andrew_williams@comcast.com>
2014-05-03 06:49:49 -0400

    Fix startup script

--- src/startup.bash
+++ src/startup.bash
@@ -1,20 +1,13 @@
-#! /bin/sh -f
-# Written by Furey.
-# With additions from Tony.
-# With changes from Kahn.
-# Ported to 'sh' by Loki for those who don't have 'csh'.
-# Thanks to Alan and Grodyn for thier help on the redirecting stuff - Loki
+#!/bin/bash -f
 
-# Set the port number.
 if [ "$1" != "" ]
 then
-         port=$1 
+    port=$1
 else
-         port=4000
+    port=4000
 fi
 
-# Change to area directory.
-cd ../area
+pushd ../area
 
 # Set limits. (cannot be used in a 'sh' script - Loki)
 # nohup
@@ -22,12 +15,11 @@ cd ../area
 # limit stack 1024k
 if [ -r shutdown.txt ]
 then
-         rm -f shutdown.txt
+    rm -f shutdown.txt
 fi
 
 while [ 1 ]
 do
-
     # If you want to have logs in a different directory,
     #   change the 'logfile=' line to reflect the directory name.
     # (It isn't used but I let it in anyway)
@@ -37,33 +29,32 @@ do
         logfile=../log/$index.log
         if [ -r $logfile ]
         then
-                 # If you don't have bc you have a problem... :(
-                 index=`echo $index + 1|bc`
+            # If you don't have bc you have a problem... :(
+            index=`echo $index + 1|bc`
         else
-                 break
+            break
         fi
     done
 
     # Update to new code if possible.
     if [ -r ../src/envy.new ]
     then
-	\mv ../src/envy2 ../src/envy.old
-	\mv ../src/envy.new ../src/envy2
+        \mv ../src/envy ../src/envy.old
+        \mv ../src/envy.new ../src/envy
     fi
 
-    # Run envy.
-    # Thanks to Alan and Grodyn for their help on the redirecting stuff - Loki
     ../src/envy $port >$logfile 2>&1
 
     if [ -r core ]
     then
-	\mv core ../src/
+        \mv core ../src/
     fi
 
     # Restart, giving old connections a chance to die.
     if [ -r shutdown.txt ]
     then
         rm -f shutdown.txt
+        popd
         exit 0
     fi
     sleep 10
